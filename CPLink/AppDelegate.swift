b34cfd343d5980ee3589b5fef91d92f93e88a4fa
nishanth28 <pnishanth28@gmail.com>
2017-02-11 21:22:22 -0500

    Initial Commit

---
+++ CPLink/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CPLink
+//
+//  Created by Nishanth P on 2/11/17.
+//  Copyright © 2017 Nishapp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CPLink/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CPLink
+//
+//  Created by Nishanth P on 2/11/17.
+//  Copyright © 2017 Nishapp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
